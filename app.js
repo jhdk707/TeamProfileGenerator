cb1cef8d6720309c8b884b7d4c6c60744c97cbfb
jhdk707 <jessehudak707@outlook.com>
2023-02-20 23:45:29 -0800

    manager logic start build

--- app.js
+++ app.js
@@ -11,4 +11,38 @@ const DIST_DIR = path.resolver(__dirname, "output");
 const outputPath = path.join(DIST_DIR, "index.html");
 
 const render = require("./lib/htmlRender");
+const { run } = require("jest");
+
+const teamProfile = [];
+
+const managerPrompt = () => {
+    return new Promise((res, rej) => {
+        inquirer
+            .prompt([{
+                type: "input",
+                message: `What is the managers name?`,
+                name: "name",
+            },
+            {
+                type: "input",
+                message: `What is the managers id?`
+                name: "id",
+            }, 
+            {
+                type: "input",
+                message: "What is the managers email?"
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the managers office number?"
+                name: "officeNumber",
+            },
+            ]).then(answers => {
+                const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+                teamProfile.push(manager);
+                res();
+            });
+    });
+}
 
