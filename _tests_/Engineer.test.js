9b1ef3b6e65b8287ff7711185bf32aadef416129
Doomtower <jessehudak707@outlook.com>
2023-02-22 22:19:32 -0800

    fixed typo

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,19 +2,19 @@ const Engineer = require("../lib/Engineer");
 const GitHubUser = "testUsername";
 
 test("Can get gitHub acct with constructor", () => {
-    const testValue = "GitHubUser";
-     const emp = new Engineer("Foo", 1, "test@test.com", testValue);
-     expect(emp.github).toBe(testValue);
+  const testValue = "GitHubUser";
+  const emp = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(emp.github).toBe(testValue);
 });
 
-test ("Can get Engineer Role with getRole function", () => {
-    const testValue = "Engineer";
-    const emp = new Engineer("Foo", 1, "test@test.com", GitHubUser);
-    expect(emp.getRole()).toBe(testValue);
+test("Can get Engineer Role with getRole function", () => {
+  const testValue = "Engineer";
+  const emp = new Engineer("Foo", 1, "test@test.com", GitHubUser);
+  expect(emp.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username with github function", () => {
-    const testValue = "GitHubUser";
-    const emp = new Engineer("Foo", 1, "test@test.com", testValue);
-    expect(emp.getGithub()).toBe(testValue);
+  const testValue = "GitHubUser";
+  const emp = new Engineer("Foo", 1, "test@test.com", testValue);
+  expect(emp.getGithub()).toBe(testValue);
 });
